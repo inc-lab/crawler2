c39a11ae9deedc62b62e0cf22e84051d4234f7fc
konst <mr.kamischin@mail.ru>
2020-08-28 15:37:36 +0300

    first commit

---
+++ config/docs.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "основной" : "1eFPqqcN6Pkaob6Zsq7XQGQ552cluftGq6AIVfJP3T3E",
+  "новый"  : "1JXOXxUFSaszZDmwc-9HVM7DNhQ-taDjcbadR2QG3xHw",
+  "oli"    : "1OZOaqFNXBZoH6MIgK_fZSN5h7zLdeHtmUltkjYOfZNc",
+  "vb"    : "1bbmCnfpZ6JodnNbWdBAqy6B5Y1RRvhg0NNVCcfkUITM",
+  "timo"    : "1oFJ57iuLTwSVD_hKaGzSodjhYxkLM6eizSWVmzoduI4",
+  "акварель"    : "1Tqm2A8FS3oMUjIvblXjbW348s7zIBTAnBXd49z67jEs",
+  "ПЭК_Рока"    : "1L7B_LgXKwfZBR0OpKzFf4UYPqc0UkMo2Tto9r8AZsyE",
+ "test" : "fx"
+  }
